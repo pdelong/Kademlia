27dccbc2efe097ce30ad1ef7f18f97fe1b57a62f
Peter DeLong <pdelong@princeton.edu>
2018-03-16 16:02:07 -0400

    Add RPC skeleton

--- cmd/kademlia_node/main.go
+++ cmd/kademlia_node/main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/peterdelong/kademlia"
+	"os"
 )
 
+// usage: kademlia_node <node_addr> [bootstrap_addr]
 func main() {
-	node := kademlia.NewNode("140.180.129.24", 7878)
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("usage: kademlia_node <node_addr> [bootstrap_addr]")
+		return
+	}
+
+	addr := args[0]
+	//bootstrapAddr := args[1]
+
+	node := kademlia.NewNode(addr)
 	fmt.Println(node)
+
+	node.Run()
 }
--- kademlia.go
+++ kademlia.go
@@ -3,29 +3,70 @@ package kademlia
 import (
 	"container/list"
 	"crypto/sha1"
+	//	"errors"
 	"fmt"
 	"math/big"
+	"net"
+	"net/http"
+	"net/rpc"
 )
 
 // An entry in the k-bucket
 type NodeEntry struct {
 	id   *big.Int
-	ip   string
-	port int
+	addr *net.IPAddr
 }
 
 type Node struct {
 	id       *big.Int
-	address  string
-	port     int
+	addr     *net.TCPAddr
 	kBuckets []list.List // TODO: Might want array of arrays
 }
 
+type PingArgs struct {
+}
+
+type PingReply struct {
+}
+
+type StoreArgs struct {
+}
+
+type StoreReply struct {
+}
+
+type FindValueArgs struct {
+}
+
+type FindValueReply struct {
+}
+
+type FindNodeArgs struct {
+}
+
+type FindNodeReply struct {
+}
+
+func (self *Node) Ping(args PingArgs, reply *PingReply) error {
+	return nil
+}
+
+func (self *Node) Store(args StoreArgs, reply *StoreReply) error {
+	return nil
+}
+
+func (self *Node) FindValue(args FindValueArgs, reply *FindValueReply) error {
+	return nil
+}
+
+func (self *Node) FindNode(args FindNodeArgs, reply *FindNodeReply) error {
+	return nil
+}
+
 func (self *Node) String() string {
-	return fmt.Sprintf("Node: (id = %v) (address = %s:%d) (kBuckets = %v)",
+	return fmt.Sprintf("Node: (id = %v) (address = %v) (kBuckets = %v)",
 		self.id,
-		self.address,
-		self.port,
+		self.addr,
 		self.kBuckets)
 }
 
@@ -34,16 +75,56 @@ func (self *Node) distanceTo(other NodeEntry) *big.Int {
 	return big.NewInt(0).Xor(self.id, other.id)
 }
 
-func NewNode(address string, port int) *Node {
+func NewNode(address string) *Node {
 	node := new(Node)
-	node.address = address
-	node.port = port
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return nil
+	}
+
+	node.addr = addr
 
 	// #eww
-	hash := sha1.Sum([]byte(fmt.Sprintf("%s:%d", address, port)))
+	hash := sha1.Sum([]byte(fmt.Sprintf("%s", address)))
 
 	node.id = big.NewInt(0)
 	node.id.SetBytes(hash[:])
 
 	return node
+}
+
+func (self *Node) Run() {
+	nodeRPC := &NodeRPC{self}
+	rpc.Register(nodeRPC)
+	rpc.HandleHTTP()
+
+	l, e := net.ListenTCP("tcp", self.addr)
+	if e != nil {
+		return
+	}
+	http.Serve(l, nil)
+}
+
+func (self *NodeRPC) Ping(args PingArgs, reply *PingReply) error {
+	self.node.Ping(args, reply)
+	return nil
+}
+
+func (self *NodeRPC) Store(args StoreArgs, reply *StoreReply) error {
+	self.node.Store(args, reply)
+	return nil
+}
+
+func (self *NodeRPC) FindValue(args FindValueArgs, reply *FindValueReply) error {
+	self.node.FindValue(args, reply)
+	return nil
+}
+
+func (self *NodeRPC) FindNode(args FindNodeArgs, reply *FindNodeReply) error {
+	self.node.FindNode(args, reply)
+	return nil
+}
+
+type NodeRPC struct {
+	node *Node
 }
