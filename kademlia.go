4087d9104c0a04c5417354e2510dcab962091677
em-tang <emt4@princeton.edu>
2018-03-30 16:29:00 -0400

    pings implemented, weird big.Int pointer issue

--- kademlia.go
+++ kademlia.go
@@ -4,16 +4,19 @@ import (
 	"container/list"
 	"crypto/sha1"
 	"fmt"
+	"log"
 	"math/big"
 	"net"
 	"net/http"
 	"net/rpc"
+	"os"
+	"time"
 )
 
 // An entry in the k-bucket
 type NodeEntry struct {
-	id   big.Int
-	addr net.IPAddr
+	Id   big.Int
+	Addr net.TCPAddr
 }
 
 type Node struct {
@@ -21,11 +24,12 @@ type Node struct {
 	addr     net.TCPAddr
 	ht       map[string][]byte
 	kBuckets []list.List // TODO: Might want array of arrays
+	logger   *log.Logger
 	// TODO: routing table
 }
 
 type PingArgs struct {
-	Source big.Int
+	Source NodeEntry
 }
 
 type PingReply struct{}
@@ -58,6 +62,8 @@ type FindNodeReply struct {
 }
 
 func (self *Node) Ping(args PingArgs, reply *PingReply) error {
+
+	self.logger.Printf("Ping from %s", args.Source.Addr)
 	// TODO: Update k-bucket based on args.Source
 	return nil
 }
@@ -85,7 +91,7 @@ func (self *Node) String() string {
 
 // Return XOR distance between self and other
 func (self *Node) distanceTo(other *NodeEntry) *big.Int {
-	return big.NewInt(0).Xor(&self.id, &other.id)
+	return big.NewInt(0).Xor(&self.id, &other.Id)
 }
 
 func NewNode(address string) *Node {
@@ -103,21 +109,57 @@ func NewNode(address string) *Node {
 	node.id = *big.NewInt(0)
 	node.id.SetBytes(hash[:])
 
+	node.logger = log.New(os.Stdout, "INFO:  ", log.Ldate|log.Ltime|log.Lshortfile)
 	return node
 }
 
-func (self *Node) Run() {
+func (self *Node) Run(toPing string) {
+
 	nodeRPC := &NodeRPC{self}
 	rpc.Register(nodeRPC)
 	rpc.HandleHTTP()
+
+	toPingAddr, err := net.ResolveTCPAddr("", toPing)
+	//TODO: handle err
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	l, e := net.ListenTCP("tcp", &self.addr)
 	if e != nil {
 		return
 	}
+
+	// periodically ping
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for range ticker.C {
+			self.DoPing(*toPingAddr)
+		}
+	}()
 	http.Serve(l, nil)
 }
 
+// DoPing sends RPC, Ping is the one called
+func (self *Node) DoPing(dest net.TCPAddr) {
+	client, err := rpc.DialHTTP("tcp", dest.String())
+	// TODO : handle err
+	if err != nil {
+		self.logger.Printf("Ping RPC to %s failed %s", dest, err)
+		return
+	}
+
+	nodeEntry := NodeEntry{Id: self.id, Addr: self.addr}
+	args := PingArgs{nodeEntry}
+	var reply PingReply
+	err = client.Call("NodeRPC.Ping", args, &reply)
+	self.logger.Printf("DoPing called on %s", dest.String())
+	if err != nil {
+		self.logger.Printf("Ping RPC to %s failed %s", dest, err)
+	}
+}
+
+// do not touch
 func (self *NodeRPC) Ping(args PingArgs, reply *PingReply) error {
 	self.node.Ping(args, reply)
 	return nil
