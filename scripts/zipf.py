4a78b6315b9be44df316282f5c41da267fc07c0c
Peter DeLong <pdelong@princeton.edu>
2018-05-06 16:09:02 -0400

    Have Zipf sample instead of returning index for easier usage in selecting keys

--- scripts/zipf.py
+++ scripts/zipf.py
@@ -4,13 +4,27 @@ import bisect
 import math
 
 class Zipf:
-    def __init__(self, n, alpha):
+    def __init__(self, arr, alpha):
+        n = len(arr)
         tmp = [1. / math.pow(float(i), alpha) for i in range(1, n+1)]
         zeta = functools.reduce(lambda sums, x: sums + [sums[-1] + x], tmp, [0])
 
+        self.arr = arr
         self.distMap = [x / zeta[-1] for x in zeta]
 
     def next(self):
         u = random.random()
+        index = bisect.bisect(self.distMap, u)
 
-        return bisect.bisect(self.distMap, u)
+        return self.arr[index-1]
+
+if __name__ == '__main__':
+    n = []
+    for _ in range(1, 100):
+        n = n + [random.random()]
+
+    a = 3
+    dist = Zipf(n, a)
+
+    for _ in range(1, 1000):
+        print(dist.next())
