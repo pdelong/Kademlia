6c94d810f340563764d60f92a0b211e2037d9ebd
Peter DeLong <pdelong@princeton.edu>
2018-05-10 12:32:45 -0400

    Begin integration of test script into slurm script

--- slurm_stuff/run_kademlia.sh
+++ slurm_stuff/run_kademlia.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 ips=($(hostname -I))
-cd /home/pdelong/go/src/github.com/peterdelong/kademlia/cmd/kademlia_node
+PORT=8001
+ROOT=/home/pdelong/go/src/github.com/peterdelong/kademlia
 
-./kademlia_node ${ips[0]}:8000 nb > /home/pdelong/go/src/github.com/peterdelong/kademlia/logs/${ips[0]} 
+$ROOT/cmd/kademlia_node/kademlia_node ${ips[0]}:$PORT nb > $ROOT/logs/${ips[0]}.log &
+
+sleep 10
+
+$ROOT/scripts/test.py ${ips[0]}:$PORT shutdown > $ROOT/logs/${ips[0]}.test.log
+
+# Make sure it's dead
+kill $! 2> /dev/null
+if [[ $? == '0' ]]; then
+    echo "Node had to be killed (shutdown probably failed)"
+    exit 1
+fi
--- slurm_stuff/run_slurm.sh
+++ slurm_stuff/run_slurm.sh
@@ -4,18 +4,21 @@ if [ "$#" -ne 1 ] ; then
     exit 1
 fi
 
+ROOT=/home/pdelong/go/src/github.com/peterdelong/kademlia
+PORT=8000
+
 # rebuild the project and save the bootstrap node's address
-cd /home/pdelong/go/src/github.com/peterdelong/kademlia/cmd/kademlia_node
-echo $1:8000 > bootstrap_nodes
+cd $ROOT/cmd/kademlia_node
+echo $1:$PORT > bootstrap_nodes
 go build
 
 # start up the bootstrap node and save the id for later
-./kademlia_node $1:8000 b > /home/pdelong/go/src/github.com/peterdelong/kademlia/logs/bootstrap_log &
+./kademlia_node $1:$PORT b > $ROOT/logs/bootstrap.log &
 jobid=$!
 
 # start up the other nodes and wait until they finish, so we can kill
 # the bootstrap node
-cd /home/pdelong/go/src/github.com/peterdelong/kademlia/slurm_stuff
+cd $ROOT/slurm_stuff
 sbatch --wait start_nodes.cmd
 
 # kill the bootstrap node
